1eb522a9c8701ebf9953ac0e06db6bee4d1eb54f
Ayeong Lee <dgh03207@gmail.com>
2022-02-25 20:36:24 +0900

    Update :conflict 해결

--- src/Baekjoon/java/silver/boj2847.java
+++ src/Baekjoon/java/silver/boj2847.java
@@ -1,29 +1,14 @@
 package Baekjoon.java.silver;
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-<<<<<<< HEAD
-=======
 //게임을 만든 동준이
->>>>>>> 2cc864bebbfa3312f72b774bd0ccef8908aa4a1f
 public class boj2847 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-<<<<<<< HEAD
-        int[] scores = new int[N];
-        int answer = 0;
-        int prev = 0;
-        for (int i = 0; i < N; i++) {
-//            int scores = Integer.parseInt(br.readLine());
-
-        }
-
-
-
-    }
-=======
         int answer = 0;
         int[] scores = new int[N];
 
@@ -48,5 +33,4 @@ public class boj2847 {
 
     }
 
->>>>>>> 2cc864bebbfa3312f72b774bd0ccef8908aa4a1f
 }
