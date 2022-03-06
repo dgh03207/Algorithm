1c702cca56ba250d37605c0996f8d8a0cd7f5379
Ayeong Lee <dgh03207@gmail.com>
2022-03-06 23:08:37 +0900

    Update : boj2469

--- src/Baekjoon/java/silver/boj2469.java
+++ src/Baekjoon/java/silver/boj2469.java
@@ -5,41 +5,54 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class boj2469 {
-
-    static boolean[][] ladder;
-    static int target,k,n;
-    static String middle[];
+    static int targetDepth,answer[];
+    static String alphabet[],board[][];
     public static void main(String[] args) throws Exception{
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        k = Integer.parseInt(br.readLine());
-        n = Integer.parseInt(br.readLine());
-        String[] answer = br.readLine().split("");
-        System.out.println(Arrays.toString(answer));
-        boolean[][] ladder = new boolean[n][k];
-        middle = new String[k];
-        for (int i = 0; i < n; i++) {
-            String[] line = br.readLine().split("");
-            if(line[0].equals("?")) target = i;
-            for (int j = 0; j < k-1; j++) {
-                if(line[j].equals("-")) ladder[i][j]=true;
-            }
+        int K = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        targetDepth = 0;
+        board = new String[N][K-1];
+        alphabet = new String[N];
+        answer= new int[K-1];
+        for (int i = 0; i < K; i++) {
+            alphabet = br.readLine().split("");
         }
 
-        for (int i = 0; i < k-1; i++) {
-            ghostleg(n-1,i,answer[i]);
+        for (int i = 0; i < K; i++) {
+            board[i] = br.readLine().split("");
+            if(board[i][0].equals("?")) targetDepth=i;
         }
+
+
     }
-    private static boolean isRange(int n1,int k1){
-        return n1>=0&&n1<n&&k1>=0&&k1<k;
-    }
-    private static void ghostleg(int a, int b,String word){
-        if(a==target){
-            middle[b] = word;
+
+    private static void getLadder(int now,int depth,int start,int target){
+        if(depth<0){
+            if(now==start){
+
+            }
             return;
         }
 
-        if(isRange(a-1,b-1)&&ladder[a-1][b-1]) ghostleg(a-1,b-1,word);
-        if(isRange(a-1,b)&&ladder[a-1][b]) ghostleg(a-1,b,word);
+        if(depth==targetDepth){
+
+            board[targetDepth][now-1]="-";
+            getLadder(now-1,depth-1,start,now);
+            board[targetDepth][now]="-";
+            getLadder(now+1,depth-1,start,now);
+            board[targetDepth][now]="*";
+            board[targetDepth][now-1]="*";
+            getLadder(now,depth-1,start,now);
+        }else{
+            if(now>0&&board[depth][now-1].equals("-")){
+                getLadder(now-1,depth-1,start,target);
+            }else if(board[depth][now].equals("-")){
+                getLadder(now+1,depth-1,start,target);
+            }else if(board[depth][now].equals("*")&&board[depth][now-1].equals("*")){
+                getLadder(now,depth-1,start,target);
+            }
+        }
     }
+
 }
