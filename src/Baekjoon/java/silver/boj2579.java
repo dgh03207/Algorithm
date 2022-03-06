e2d641247d68a23c93b4cbd0b35f657ea424254d
Ayeong Lee <dgh03207@gmail.com>
2022-03-06 20:33:44 +0900

    Add : boj files

--- src/Baekjoon/java/silver/boj2579.java
+++ src/Baekjoon/java/silver/boj2579.java
@@ -1,8 +1,17 @@
 package Baekjoon.java.silver;
 
-public class boj2579 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+//계단오르기
+public class boj2579 {
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N];
+        for (int i = 0; i < N; i++) {
+            score[i] = Integer.parseInt(br.readLine());
+        }
 
     }
 
