a0a4269d8f87d3dfac496088e0bed7c20a15fc34
Ayeong Lee <dgh03207@gmail.com>
2022-03-06 21:52:25 +0900

    Update : boj14888

--- src/Baekjoon/java/silver/boj14888.java
+++ src/Baekjoon/java/silver/boj14888.java
@@ -2,49 +2,66 @@ package Baekjoon.java.silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 //연산자 끼워넣기
 public class boj14888 {
-	static int N = 0;
-	static boolean selected[];
+	static int answer,N,nums[],operators[],answer2;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		nums  = new int[N];
+		operators = new int[4];
+		answer = Integer.MIN_VALUE;
+		answer2 = Integer.MAX_VALUE;
+// 		+,-,x,/순서
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		selected = new boolean[N-1];
-		int[] nums = new int[N];
-		int[] operators = new int[4];
-		st = new StringTokenizer(br.readLine());
-		
 		for (int i = 0; i < N; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
 		}
-
-		Arrays.sort(nums);
-		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < 4; i++) {
-			operators[i] = Integer.parseInt(st.nextToken());
+			operators[i]=(Integer.parseInt(st.nextToken()));
 		}
-		
-		int Max = nums[N-1];
-		int Min = nums[N-1];
-		
-		//max
-		int answer = operators[N-1];
-	
-		System.out.println(Max);
-		
-		
-		combi(0,0);
+		getResult(nums[0],1);
+		sb.append(answer+"\n");
+		sb.append(answer2);
+
+		System.out.println(sb.toString());
+	}
+
+	public static void getResult(int result,int index){
+		if(index==N){
+			if(answer < result){
+				answer = result;
+			}
+			if(answer2 > result){
+				answer2 = result;
+			}
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			int next = result;
+			if(operators[i]>0) {
+				if (i == 0) {
+					next += nums[index];
+				} else if (i == 1) {
+					next -= nums[index];
+				} else if (i == 2) {
+					next *= nums[index];
+				} else if (i == 3) {
+					if (next < 0) {
+						next = -1 * (Math.abs(next) / nums[index]);
+					} else {
+						next /= nums[index];
+					}
+				}
+				operators[i]-=1;
+				getResult(next, index + 1);
+				operators[i]+=1;
+			}
+		}
 	}
 	
-	private static void combi(int n,int start) {
-		
-		
-	}
 }
