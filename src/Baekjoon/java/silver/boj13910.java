9100007fcdec3d645549421e307e803dedfd2d94
Ayeong Lee <dgh03207@gmail.com>
2022-03-11 19:23:59 +0900

    update : boj13910

--- src/Baekjoon/java/silver/boj13910.java
+++ src/Baekjoon/java/silver/boj13910.java
@@ -2,25 +2,24 @@ package Baekjoon.java.silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class boj13910 {
-    static int N, mywok[], order, answer;
+    static int N, mywok[], order, answer, dp[];
     static List<Integer> wok = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        answer = Integer.MAX_VALUE;
+        answer = -1;
         order = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         mywok = new int[N];
-
+        dp = new int[order + 1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             mywok[i] = Integer.parseInt(st.nextToken());
@@ -30,26 +29,31 @@ public class boj13910 {
         }
 
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
+        for (int i = 0; i <N; i++) {
+            for (int j = i+1; j < N; j++) {
                 if (mywok[i] + mywok[j] <= order) {
                     wok.add(mywok[i] + mywok[j]);
                 }
             }
 
-            wok = wok.stream().sorted(Collections.reverseOrder()).distinct().collect(Collectors.toList());
-            cook(0, 0);
-            System.out.println(answer);
         }
+        wok = wok.stream().sorted().collect(Collectors.toList());
+        cook();
+        System.out.println(dp[order]);
 
     }
-    public static void cook(int result,int cnt){
-        for (int i = 0; i < wok.size(); i++) {
-            if (result + wok.get(i) < order) {
-                cook(result+wok.get(i),cnt+1);
-            }else if(result==wok.get(i)){
-                if(cnt<answer) answer = cnt+1;
-                break;
+
+    public static void cook() {
+
+        for (int i = 0; i < order+1; i++) {
+            for (int j = 0; j < wok.size(); j++) {
+                if(i<wok.get(j)) break;
+                else if(i==wok.get(j)){
+                    dp[i]=1;
+                    break;
+                }else if(dp[i-wok.get(j)]!=Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i],dp[i-wok.get(j)]+1);
+                }
             }
         }
     }
